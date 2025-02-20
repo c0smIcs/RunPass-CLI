0ae07a7fa3cbef61ab833b22721689c98d2c4d59
cosmIcs <alankemov@gmail.com>
2025-02-20 16:15:00 +0300

    добавлено логирование: slog.Debug и slog.Warn

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,20 +1,19 @@
 package generator
 
 import (
+	"log/slog"
 	"math/rand"
 	"time"
 )
 
 // GeneratePassword - генерирует случайный пароль на основе заданных параметров
 func GeneratePassword(length int, includeLetters, includeNumbers, includeSymbols bool) string {
-	// Создаем локальный генератор соучайных чисел
 	randomGenerator := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	var numbers = "0123456789"
 	var symbols = "!@#$%^&*()-_=+[]{}|;:,.<>/?`~"
 
-	// собираем строку доступных символов
 	var availableChars string
 	if includeLetters {
 		availableChars += letters
@@ -26,9 +25,8 @@ func GeneratePassword(length int, includeLetters, includeNumbers, includeSymbols
 		availableChars += symbols
 	}
 
-	// Если доступные символы не определены, возвращаем пустую строку
 	if len(availableChars) == 0 {
-		return "доступные символы не определены"
+		slog.Warn("доступные символы не определены", "letters", includeLetters, "numbers", includeNumbers, "symbols", includeSymbols)
 	}
 
 	password := make([]byte, length)
@@ -37,5 +35,6 @@ func GeneratePassword(length int, includeLetters, includeNumbers, includeSymbols
 		password[i] = availableChars[randomIndex]
 	}
 
+	slog.Debug("Пароль успешно сгенерирован внутри функции", "password", string(password))
 	return string(password)
 }
